bcc626b728a4d68012b587cbe389dd9104121332
Lautaro <lautarobazan.info@gmail.com>
2026-09-25 16:58:53 -0300

    reordene un poco y elimine archivos sobrantes

--- probando.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(api_url)
-
-# Imprimimos el resultado en la consola para poder verlo
-print(response.json())
